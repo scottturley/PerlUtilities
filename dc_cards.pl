5829bf6a61569115f10da02f6b457e59c6851a02
Scott Turley <sturley@paypal.com>
2013-09-20 15:50:50 -0700

    first commit

---
+++ dc_cards.pl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/perl
+
+# http://perlmeme.org/howtos/perlfunc/split_function.html
+# http://www.troubleshooters.com/codecorn/littperl/perlfile.htm
+# http://perl.about.com/od/perltutorials/a/readwritefiles.htm
+# http://www.tutorialspoint.com/perl/perl_arrays.htm
+
+use strict;
+use warnings;
+
+my @dc_ids;
+
+open(MYFILE, 'dc_cards.txt');
+open (OUTFILE, '>dc_cards_output.txt');
+
+while (<MYFILE>)
+{
+	chomp;
+	my @values = split(',', $_);
+
+	print OUTFILE "$values[1], $values[0]\n";
+}
+
+close(OUTFILE);
+close(MYFILE);
+
